9b25a6b148cf64289875f10deb68adfe0c130712
David Arenas <darensr@gmail.com>
2019-01-21 17:38:17 +0100

    chore(app): remove unused files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grommet, Box, Paragraph, Anchor } from 'grommet';
+import { grommet } from 'grommet/themes';
+import { Reactjs as Icon } from 'grommet-icons';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
+      <Grommet theme={grommet}>
+        <Box fill pad="xlarge" align="center">
+          <Icon className="App-logo" alt="logo" size="xlarge" color="brand" />
+          <Paragraph>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
+          </Paragraph>
+          <Anchor
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
-        </header>
-      </div>
+          </Anchor>
+        </Box>
+      </Grommet>
     );
   }
 }
