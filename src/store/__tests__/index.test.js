07d2dbd400e7ff2ede82f74474d5669ef08a7b6d
David Arenas <darensr@gmail.com>
2019-01-25 09:57:56 +0100

    test(router): test redirections

--- src/store/__tests__/index.test.js
+++ src/store/__tests__/index.test.js
@@ -1,10 +1,30 @@
+import { unprotect } from 'mobx-state-tree';
 import store from '..';
+
+unprotect(store);
 
 it('has the right properties', () => {
   expect(store).toMatchSnapshot();
 });
 
 it('changes window location after using openPage actions', () => {
+  // Before login
+  store.router.openSitesPage();
+  expect(window.location.pathname).toBe('/login');
+  store.router.openNewSitePage();
+  expect(window.location.pathname).toBe('/login');
+  store.router.openSitePage({ id: 'blog-frontity-com' });
+  expect(window.location.pathname).toBe('/login');
+  store.router.openSitePageById('test-frontity-io');
+  expect(window.location.pathname).toBe('/login');
+  store.router.openLogInPage();
+  expect(window.location.pathname).toBe('/login');
+  store.router.openSignUpPage();
+  expect(window.location.pathname).toBe('/signup');
+
+  store.access.isLoggedIn = true;
+
+  // After login
   store.router.openSitesPage();
   expect(window.location.pathname).toBe('/');
   store.router.openNewSitePage();
@@ -14,7 +34,7 @@ it('changes window location after using openPage actions', () => {
   store.router.openSitePageById('test-frontity-io');
   expect(window.location.pathname).toBe('/site/test-frontity-io');
   store.router.openLogInPage();
-  expect(window.location.pathname).toBe('/login');
+  expect(window.location.pathname).toBe('/');
   store.router.openSignUpPage();
-  expect(window.location.pathname).toBe('/signup');
+  expect(window.location.pathname).toBe('/');
 });
