45fe11f1e6a59b8577c4ae385aabe9702a7a6abf
David Arenas <darensr@gmail.com>
2019-01-25 09:59:02 +0100

    feat(sites-page): add top bar with user and logout button

--- src/components/Sites/index.js
+++ src/components/Sites/index.js
@@ -2,26 +2,44 @@ import React from 'react';
 import { Box, Heading, Text, Anchor, Button } from 'grommet';
 import wrap from 'mobx-react-wrapper/dist/mobx-react-wrapper.js';
 
-const Sites = ({ openSitePageById, openNewSitePage, openLogInPage }) => (
-  <Box fill pad="xlarge" gap="xlarge">
-    <Heading>Sites</Heading>
-    <Text>
-      <Anchor onClick={() => openSitePageById('test-frontity-io')}>
-        test-frontity-io
-      </Anchor>
-    </Text>
-    <Text>
-      <Anchor onClick={() => openSitePageById('blog-frontity-com')}>
-        blog-frontity-com
-      </Anchor>
-    </Text>
-    <Button primary label="new site" onClick={openNewSitePage} />
-    <Button label="logout" onClick={openLogInPage} />
-  </Box>
+const Sites = ({ user, openSitePageById, openNewSitePage, logOut }) => (
+  <>
+    <Box
+      fill="horizontal"
+      height="60px"
+      background="brand"
+      direction="row"
+      justify="between"
+      align="center"
+      pad="small"
+    >
+      <Text>
+        {user.name} - {user.email}
+      </Text>
+      <Button label="logout" onClick={logOut} />
+    </Box>
+    <Box fill pad="xlarge">
+      <Box width="large" gap="medium" align="start">
+        <Heading>Sites</Heading>
+        <Text>
+          <Anchor onClick={() => openSitePageById('test-frontity-io')}>
+            test-frontity-io
+          </Anchor>
+        </Text>
+        <Text>
+          <Anchor onClick={() => openSitePageById('blog-frontity-com')}>
+            blog-frontity-com
+          </Anchor>
+        </Text>
+        <Button primary label="new site" onClick={openNewSitePage} />
+      </Box>
+    </Box>
+  </>
 );
 
 export default wrap.default(store => ({
   openSitePageById: store.router.openSitePageById,
   openNewSitePage: store.router.openNewSitePage,
-  openLogInPage: store.router.openLogInPage,
+  logOut: store.access.logOut,
+  user: store.user,
 }))(Sites);
